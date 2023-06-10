6c5501d3f73e40072e6bf42831e9762a59d124be
Em Dash <mbyx.dev@gmail.com>
2023-06-10 20:22:14 +0500

    Visualizing the normals of the surface of a sphere.

--- app/Ray.hs
+++ app/Ray.hs
@@ -15,17 +15,19 @@ at :: Ray -> Double -> Point
 (Ray origin direction) `at` t = origin + (multScalar direction t)
 
 rayColor :: Ray -> Color
-rayColor ray @ (Ray _ direction) = if hitsSphere (Vec3 0.0 0.0 (-1.0)) 0.5 ray
-    then Vec3 1.0 0.0 0.0
-    else (
-    ((Vec3 1.0 1.0 1.0) `multScalar` (1.0 - t)) +
-    ((Vec3 0.5 0.7 1.0) `multScalar` t))
+rayColor ray@(Ray _ direction) = 
+    let collisionPoint = hitsSphere (Vec3 0.0 0.0 (-1.0)) 0.5 ray
+        n = unit $ ((ray `at` t) - (Vec3 0.0 0.0 (-1.0)))
+    in
+        if collisionPoint > 0.0
+        then Vec3 (getX n + 1) (getY n + 1) (getZ n + 1) `multScalar` 0.5
+        else (((Vec3 1.0 1.0 1.0) `multScalar` (1.0 - t)) + ((Vec3 0.5 0.7 1.0) `multScalar` t))
     where unitDirection = unit direction
           t = 0.5 * ((getY unitDirection) + 1.0)
 
 type Radius = Double
-hitsSphere :: Point -> Radius -> Ray -> Bool
-hitsSphere center radius ray = discriminant > 0
+hitsSphere :: Point -> Radius -> Ray -> Double
+hitsSphere center radius ray = if discriminant < 0 then -1.0 else (-b - (sqrt discriminant)) / (2.0 * a)
     where discriminant = (b * b) - (4 * a * c)
           a = dotProduct (getDirection ray) (getDirection ray)
           b = 2.0 * (dotProduct oc (getDirection ray))
