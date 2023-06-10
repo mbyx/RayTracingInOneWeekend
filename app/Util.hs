5c6b2e7a5bf03407cab8720e3d98a5ed049b3f14
Em Dash <mbyx.dev@gmail.com>
2023-06-10 20:47:58 +0500

    Fixed cabal configuration.

--- app/Util.hs
+++ app/Util.hs
@@ -1,5 +1,10 @@
 module Util where
 
+-- Haskell, being statically typed is not very ergonomic to work
+-- with when you have to do maths. These types work in a generic
+-- way such that they take in any number as the lhs, and any fraction
+-- as the rhs.
+
 (/.) :: (Integral a, Fractional b, Integral a1) => a -> a1 -> b
 x /. y = fromIntegral x / fromIntegral y
 
